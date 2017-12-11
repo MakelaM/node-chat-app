accba66fb40a303fbfe2c381c2405c35ab75ab06
Mika <mika.m.makela@gmail.com>
2017-12-11 12:56:27 +0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -23,18 +23,20 @@ socket.on('connect', function () {
     li.text(`${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
-    
+
     jQuery('#messages').append(li);    
   });
   
   jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
   
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
       from: 'User',
       text: jQuery('[name=message]').val()
     }, function () {
-  
+        jQuery('[name=message]').val('')
     });
   });
   
@@ -43,13 +45,16 @@ socket.on('connect', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
 
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');        
         alert('Unable to fetch location.');
     });
   });
